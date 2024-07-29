6856943773dc53eb48dd0866f958b3a45eab0642
connorrewa <connorrewa13@hotmail.com>
2024-07-28 20:31:08 -0400

    clean up files

--- knightMoves.js
+++ knightMoves.js
@@ -1,27 +1,60 @@
-export default function knightMoves(start, end) {
-    
-}
+function knightMoves(start, end) {
+    const boardSize = 8;
+    const moves = [
+        [2,1],
+        [1,2],
+        [-2,1],
+        [-1,2],
+        [2,-1],
+        [1,-2],
+        [-2,-1],
+        [-1,-2]
+    ]
+    const q = [[start]];
+    console.log(q);
+    const visited = [[...start]];
 
-function dfs(start, end) {
-    let [endX, endY] = end;
-    let [startX, startY] = start;
-    console.log(startX, startY);
-    let visited = [];
-    for (let i = 0; i < 8; i++) {
-        let newRow = []
-        for (let j = 0; j < 8; j++) {
-            
-            newRow.push(0);
+    while (q.length > 0) {
+        const path = q.shift();
+        console.log(q);
+        const currentPosition = path[path.length - 1];
+
+        if(currentPosition[0] === end[0] && currentPosition[1] === end[1]) {
+            return path;
+
         }
-        visited.push(newRow);
+        moves.forEach((move) => {
+            const nextPosition = [
+                currentPosition[0] + move[0],
+                currentPosition[1] + move[1]
+            ];
+            const isVisited = visited.some((arr) => {
+                return (
+                    Array.isArray(arr) &&
+                    arr.every((item,index) => item === nextPosition[index])
+                );
+            });
+            if (
+                nextPosition[0] >= 0 && 
+                nextPosition[0] < boardSize &&
+                nextPosition[1] >= 0 &&
+                nextPosition[1] < boardSize
+            ) {
+                q.push([...path, nextPosition]);
+                visited.push(nextPosition);
+            }
+
+        })
     }
-    visited[startX][startY] = 'v';
-    visited[endX][endY] = '2';
-    console.log(visited)
-    if (start[0] === end[0] && start[1] == end[1]) {
-        return true;
-    }
-    
 }
 
-console.log(dfs([1,2], [1,4]));
+function pathPrinter(pathArray) {
+    console.log(
+        `You made it in ${pathArray.length - 1} moves! Here is your path:`
+    );
+    pathArray.forEach((item) => {
+        console.log(item);
+    })
+}
+
+pathPrinter(knightMoves([3,3], [4,3]));
